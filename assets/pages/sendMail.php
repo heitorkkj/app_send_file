ff4e6653aec3ca06db572f31e2574e8536adbb23
Heitor Santos <heitorsan548@gmail.com>
2023-03-18 19:11:41 -0300

    docs
    initialzing files in php

--- assets/pages/sendMail.php
+++ assets/pages/sendMail.php
@@ -1,4 +1,19 @@
 <?php
+    $file = $_POST["myfile"]; 
     print_r($_POST);
 
-    ?>
+    $uploaddir = '/var/www/uploads/';
+    $uploadfile = $uploaddir . basename($_FILES['myfile']['name']);
+
+    echo '<pre>';
+    if (move_uploaded_file($_FILES['myfile']['tmp_name'], $uploadfile)) {
+        echo "Arquivo válido e enviado com sucesso.\n";
+    } else {
+        echo "Possível ataque de upload de arquivo!\n";
+    }
+
+    echo 'Aqui está mais informações de debug:';
+    print_r($_FILES);
+
+    print "</pre>";
+?>
